a5bf5e4a1f57059d403295b4e4186205da066fd8
HagbardCeline <janpauldahlke@gmail.com>
2023-12-01 13:38:56 +0100

    using custom jpa query, it's naming convention and add testcase

--- src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
+++ src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
@@ -6,5 +6,9 @@ import org.springframework.data.repository.CrudRepository
 interface CourseRepository: CrudRepository<Course, Int> {
 
     //in mem database h2
+    //jpa custom querries
+    //https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+
+    fun findByNameContaining(courseName:String): List<Course>
 
 }
---
+++ src/test/int/com/kotlinspring/repository/CourseRepositoryIntTest.kt
@@ -0,0 +1,48 @@
+package com.kotlinspring.repository
+
+import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.entitiy.Course
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+@DataJpaTest // look this up in rtfm
+@ActiveProfiles("test")
+class CourseRepositoryIntTest {
+
+    @Autowired
+    lateinit var repository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+        //data
+        val listOfCouses = listOf(
+            CourseDTO(null, "My Yoga Course", "WELLNESS"),
+            CourseDTO(null, "The Big JUNIT5 Course", "TESTING"),
+            CourseDTO(null, "Another FOO Course Name", "ANOTHER"),
+        ).let {
+            it.map {
+                Course(it.id, it.name, it.category)
+            }
+        }
+
+        repository.deleteAll()
+        //persist
+        repository.saveAll(listOfCouses)
+
+
+    }
+
+    @Test
+    fun findNameContaining() {
+        val course = repository.findByNameContaining("Yoga")
+        assertEquals(1, course.size)
+
+        val courses = repository.findByNameContaining("Course")
+        assertEquals(2, courses.size)
+    }
+
+
+}
